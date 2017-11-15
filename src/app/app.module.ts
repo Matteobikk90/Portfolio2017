09069218443aef80131b72b227bb3fc8779e6f0f
“Matteo git config --global user.email “matteo.soresini@hotmail.it <matteo.soresini@hotmail.it>
2017-11-15 15:36:24 +0100

    imagesloaded, fragmentFX nd typed.js imported and working fine

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
-
+import { LoadingSectionComponent } from './components/loading-section/loading-section.component';
+import * as $ from 'jquery';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AboutMeComponent
+    AboutMeComponent,
+    LoadingSectionComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/loading-section/loading-section.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-section',
+  templateUrl: './loading-section.component.html',
+  styleUrls: ['./loading-section.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class LoadingSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
